39c4df729abe4b78ab209b8a8cc404ef6ad726c0
Algorithmica <info@algorithmica.co.in>
2016-10-15 12:07:40 -0500

    uploading linked list and set code discussed in class

---
+++ Set/src/com/alg/top20/set/Driver.java
@@ -0,0 +1,21 @@
+package com.alg.top20.set;
+
+import java.util.Random;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		HashSet hset = new HashSet();
+		
+		int limit = Integer.parseInt(args[0]);
+		Random r = new Random(100);
+		
+		for(int i = 1; i <= limit; ++i) {
+			hset.add(r.nextInt(1000));
+		}
+		System.out.println(hset.size());
+		hset.display();
+
+	}
+
+}
